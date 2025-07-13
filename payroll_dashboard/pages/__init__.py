306f584d9c93f7515070c4756e45ce5946bd80bf
0zean <42080997+0zean@users.noreply.github.com>
2025-07-13 01:51:41 -0400

    Add onboard to pages

--- payroll_dashboard/pages/__init__.py
+++ payroll_dashboard/pages/__init__.py
@@ -1,7 +1,7 @@
 from .about import about
 from .index import index
-from .profile import profile
+from .onboard import onboard
 from .settings import settings
 from .table import table
 
-__all__ = ["about", "index", "profile", "settings", "table"]
+__all__ = ["about", "index", "onboard", "settings", "table"]
