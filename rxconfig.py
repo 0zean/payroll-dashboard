1ebf74ba1065bd55d24f087e9236f622894cce4a
0zean <42080997+0zean@users.noreply.github.com>
2025-07-18 19:29:34 -0400

    Disable Sitemap XML

--- rxconfig.py
+++ rxconfig.py
@@ -3,4 +3,5 @@ import reflex as rx
 config = rx.Config(
     app_name="payroll_dashboard",
     plugins=[rx.plugins.TailwindV3Plugin()],
+    disable_plugins=['reflex.plugins.sitemap.SitemapPlugin']
 )
