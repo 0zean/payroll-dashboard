273ac1af4f8de1980fabe949c0bae6cab8ce2b94
0zean <42080997+0zean@users.noreply.github.com>
2025-07-26 03:43:35 -0400

    Explicitly set backend port for local testing

--- rxconfig.py
+++ rxconfig.py
@@ -4,5 +4,5 @@ config = rx.Config(
     app_name="payroll_dashboard",
     plugins=[rx.plugins.TailwindV3Plugin()],
     disable_plugins=["reflex.plugins.sitemap.SitemapPlugin"],
-    backend_port=8001
+    backend_port=8001,
 )
