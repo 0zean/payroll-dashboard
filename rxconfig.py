4fdf0611eff291e6ed603836ebf2b8399154caeb
0zean <42080997+0zean@users.noreply.github.com>
2025-07-26 21:52:46 -0400

    Set frontend port explicitly

--- rxconfig.py
+++ rxconfig.py
@@ -4,5 +4,6 @@ config = rx.Config(
     app_name="payroll_dashboard",
     plugins=[rx.plugins.TailwindV3Plugin()],
     disable_plugins=["reflex.plugins.sitemap.SitemapPlugin"],
+    frontend_port=3001,
     backend_port=8001,
 )
