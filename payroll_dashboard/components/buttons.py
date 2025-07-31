0fd84a98739c195001c77f1cd2cc7aaabe0e68cd
0zean <42080997+0zean@users.noreply.github.com>
2025-07-31 01:43:06 -0400

    Separate buttons

--- payroll_dashboard/components/buttons.py
+++ payroll_dashboard/components/buttons.py
@@ -3,47 +3,53 @@ from reflex.components.radix.themes.base import LiteralAccentColor
 from reflex.event import EventCallback
 
 
-def remote_button(
+def clean_button(
+    icon: str,
+    color: LiteralAccentColor,
+    event: EventCallback[*tuple[()]],
+    loading: bool,
+) -> rx.Component:
+    return rx.alert_dialog.root(
+        rx.alert_dialog.trigger(
+            rx.icon_button(
+                rx.icon(icon),
+                padding="0.5rem",
+                radius="full",
+                variant="soft",
+                color_scheme=color,
+                size="3",
+                loading=loading,
+            ),
+        ),
+        rx.alert_dialog.content(
+            rx.alert_dialog.title("Clean Master List"),
+            rx.alert_dialog.description(
+                "Are you sure? This will clear all hours and pay from the Master list in Sheets.",
+                size="2",
+            ),
+            rx.flex(
+                rx.alert_dialog.cancel(
+                    rx.button("Cancel", variant="soft", color_scheme="gray"),
+                ),
+                rx.alert_dialog.action(
+                    rx.button("Yes, Delete", color_scheme="ruby", variant="solid", on_click=event),
+                ),
+                spacing="3",
+                margin_top="16px",
+                justify="end",
+            ),
+            style={"max_width": 450},
+        ),
+    )
+
+
+def download_button(
     icon: str,
     color: LiteralAccentColor,
     hint: str,
     event: EventCallback[*tuple[()]],
     loading: bool,
-    alert_dialog: bool = False,
 ) -> rx.Component:
-    if alert_dialog:
-        return rx.alert_dialog.root(
-            rx.alert_dialog.trigger(
-                rx.icon_button(
-                    rx.icon(icon),
-                    padding="0.5rem",
-                    radius="full",
-                    variant="soft",
-                    color_scheme=color,
-                    size="3",
-                    loading=loading,
-                ),
-            ),
-            rx.alert_dialog.content(
-                rx.alert_dialog.title("Clean Master List"),
-                rx.alert_dialog.description(
-                    "Are you sure? This will clear all hours and pay from the Master list in Sheets.",
-                    size="2",
-                ),
-                rx.flex(
-                    rx.alert_dialog.cancel(
-                        rx.button("Cancel", variant="soft", color_scheme="gray"),
-                    ),
-                    rx.alert_dialog.action(
-                        rx.button("Yes, Delete", color_scheme="ruby", variant="solid", on_click=event),
-                    ),
-                    spacing="3",
-                    margin_top="16px",
-                    justify="end",
-                ),
-                style={"max_width": 450},
-            ),
-        )
     return rx.tooltip(
         rx.box(
             rx.icon_button(
